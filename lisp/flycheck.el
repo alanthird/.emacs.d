d0804f79bbbf1911e22505653c62005fc6bbfc9d
Alan Third <alan@idiocy.org>
2018-08-22 20:03:58 +0100

    Turn on Objective-C support for flycheck

--- lisp/flycheck.el
+++ lisp/flycheck.el
@@ -1,4 +1,6 @@
 (use-package flycheck
   :init
   (global-flycheck-mode)
+  (with-eval-after-load 'flycheck
+    (add-hook 'flycheck-mode-hook #'flycheck-objc-clang-setup))
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc perl)))
