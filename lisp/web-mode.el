f207c63081f3582c2daf732590e56ba63768ec90
Alan Third <alan.third@argyll-bute.gov.uk>
2023-08-29 14:46:56 +0100

    Turn off web mode auto indentation

--- lisp/web-mode.el
+++ lisp/web-mode.el
@@ -1,4 +1,5 @@
 (use-package web-mode
   :mode ("\\.phtml\\'" "\\.tpl\\'" "\\.php\\'" "\\.[gj]sp\\'"
          "\\.as[cp]x\\'" "\\.erb\\'" "\\.mustache\\'" "\\.hbs\\'"
-         "\\.djhtml\\'" "\\.html?\\'"))
+         "\\.djhtml\\'" "\\.html?\\'")
+  :init (setq web-mode-enable-auto-indentation nil))
