c0cf0f8e4f29e523d537b49177b05c588e5c3cef
Alan Third <alan.third@argyll-bute.gov.uk>
2023-09-19 13:16:39 +0100

    Fix interactive lisp modes

--- lisp/lisp-modes.el
+++ lisp/lisp-modes.el
@@ -2,36 +2,34 @@
 (use-package paredit
   :ensure t)
 
-(defvar electrify-return-match
-  "[\]}\)\"]"
-  "If this regexp matches the text after the cursor, do an \"electric\"
-  return.")
-
-(defun electrify-return-if-match (arg)
-  "If the text after the cursor matches `electrify-return-match' then
-  open and indent an empty line between the cursor and the text.  Move the
-  cursor to the new line."
-  (interactive "P")
-  (let ((case-fold-search nil))
-    (if (looking-at electrify-return-match)
-        (save-excursion (newline-and-indent)))
-    (newline arg)
-    (indent-according-to-mode)))
-
 (defun my-lisp-setup ()
   (paredit-mode t)
-  (local-set-key (kbd "RET") 'electrify-return-if-match)
   
   (turn-on-eldoc-mode)
   (eldoc-add-command
    'paredit-backward-delete
-   'paredit-close-round
-   'electrify-return-if-match))
+   'paredit-close-round))
+
+;; https://blog.ryuslash.org/archives/2023/04/12/ielm-paredit
+(defun remove-paredit-newline-keys ()
+  "Disable ‘C-j’ and ‘RET’ keybindings from ‘paredit-mode’."
+  (let ((oldmap (map-elt minor-mode-map-alist 'paredit-mode))
+        (newmap (make-sparse-keymap)))
+    (set-keymap-parent newmap oldmap)
+    (define-key newmap (kbd "RET") nil)
+    (define-key newmap (kbd "C-j") nil)
+    (make-local-variable 'minor-mode-overriding-map-alist)
+    (push `(paredit-mode . ,newmap) minor-mode-overriding-map-alist)))
+
 
 (add-hook 'emacs-lisp-mode-hook                  #'my-lisp-setup)
 (add-hook 'lisp-mode-hook                        #'my-lisp-setup)
 (add-hook 'lisp-interaction-mode-hook            #'my-lisp-setup)
 (add-hook 'scheme-mode-hook                      #'my-lisp-setup)
+(add-hook 'ielm-mode-hook                        #'my-lisp-setup)
+(add-hook 'ielm-mode-hook                        #'remove-paredit-newline-keys)
 
 (add-hook 'eval-expression-minibuffer-setup-hook
           (lambda () (paredit-mode t)))
+(add-hook 'eval-expression-minibuffer-setup-hook
+          #'remove-paredit-newline-keys)
